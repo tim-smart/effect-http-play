65feb6e3807b5e037c9c20165ba4d11f22011de0
Tim <hello@timsmart.co>
2024-08-15 21:58:00 +1200

    try security

--- src/Api/Accounts.ts
+++ src/Api/Accounts.ts
@@ -1,11 +1,26 @@
-import { ApiEndpoint, ApiGroup } from "effect-http"
-import { User } from "../Domain/User.js"
+import { ApiEndpoint, ApiGroup, Security } from "effect-http"
+import { ApiKey, User } from "../Domain/User.js"
+import { Effect, Redacted } from "effect"
+import { Accounts } from "../Accounts.js"
+
+const security = Security.bearer().pipe(
+  Security.map((token) => ApiKey.make(Redacted.make(token))),
+  Security.mapEffect((apiKey) =>
+    Accounts.findUserByApiKey(apiKey).pipe(Effect.flatten),
+  ),
+)
 
 export const accountsApiGroup = ApiGroup.make("Accounts").pipe(
   ApiGroup.addEndpoint(
     ApiEndpoint.post("createUser", "/users").pipe(
       ApiEndpoint.setRequestBody(User.jsonCreate),
       ApiEndpoint.setResponseBody(User.json),
+    ),
+  ),
+  ApiGroup.addEndpoint(
+    ApiEndpoint.get("getUserMe", "/users/me").pipe(
+      ApiEndpoint.setResponseBody(User.json),
+      ApiEndpoint.setSecurity(security),
     ),
   ),
 )
--- src/Http.ts
+++ src/Http.ts
@@ -3,12 +3,13 @@ import { NodeSwaggerFiles } from "effect-http-node"
 import { api } from "./Api.js"
 import { Accounts } from "./Accounts.js"
 import { HttpMiddleware, HttpServer } from "@effect/platform"
-import { Layer } from "effect"
+import { Effect, Layer } from "effect"
 import { NodeHttpServer } from "@effect/platform-node"
 import { createServer } from "http"
 
 export const HttpLive = RouterBuilder.make(api).pipe(
   RouterBuilder.handle("createUser", ({ body }) => Accounts.createUser(body)),
+  RouterBuilder.handle("getUserMe", (_, user) => Effect.succeed(user)),
   RouterBuilder.build,
   HttpMiddleware.cors(),
   HttpServer.serve(HttpMiddleware.logger),
